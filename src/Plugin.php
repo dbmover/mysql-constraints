fdac4ddd813f4c972252c841c84e6d123bfba096
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-15 18:30:12 +0200

    add the plugin

---
+++ src/Plugin.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @pacakge Dbmover
+ * @subpackage Mysql
+ * @subpackage Constraints
+ */
+
+namespace Dbmover\Mysql\Constraints;
+
+use Dbmover\Constraints;
+use PDO;
+
+class Plugin extends Constraints\Plugin
+{
+    protected function dropConstraint(string $table, string $constraint, string $type)
+    {
+        if ($type == 'FOREIGN KEY') {
+            $this->addOperation("ALTER TABLE $table DROP FOREIGN KEY $constraint;");
+        } elseif ($type == 'PRIMARY KEY') {
+            $info = $this->getPrimaryKeyInfo($table);
+            $this->addOperation("ALTER TABLE $table DROP PRIMARY KEY, CHANGE {$info['column_name']} {$info['column_name']} {$info['column_type']};");
+        }
+    }
+
+    protected function getPrimaryKeyInfo(string $table) : array
+    {
+        static $getIndex, $getColumn;
+        if (!isset($getIndex, $getColumn)) {
+            $getIndex = $this->loader->getPdo()->prepare(
+                "SELECT column_name FROM information_schema.statistics WHERE table_schema = ? AND table_name = ? AND index_name = 'PRIMARY'");
+            $getColumn = $this->loader->getPdo()->prepare(
+                "SELECT column_type FROM information_schema.columns WHERE table_schema = ? AND table_name = ? AND column_name = ?");
+        }
+        $getIndex->execute([$this->loader->getDatabase(), $table]);
+        $column = $getIndex->fetch(PDO::FETCH_ASSOC);
+        $getColumn->execute([$this->loader->getDatabase(), $table, $column['column_name']]);
+        $column['column_type'] = $getColumn->fetchColumn();
+        return $column;
+    }
+}
+
